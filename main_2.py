b879da1f841f76ed1b24b7180a323bcd6fd2efcd
pareksha <pareksha.manchanda@gmail.com>
2017-11-21 00:59:32 +0530

    Changes made

--- main_2.py
+++ main_2.py
@@ -13,12 +13,12 @@ driver.get("https://web.whatsapp.com/")
 
 time.sleep(15)
 search_bar = driver.find_element_by_class_name("input-search")
-search_bar.send_keys("shivam")
+search_bar.send_keys("deepinder")
 search_bar.send_keys(Keys.ENTER)
 
-for x in range(72525):
+for x in range(50):
     msg_box = driver.find_element_by_class_name("pluggable-input-body")
-    msg_box.send_keys("All the best for papers bro...!!!")
+    msg_box.send_keys("All the best for exams bro...!!!")
     msg_box.send_keys(Keys.ENTER)
 
 driver.close()
